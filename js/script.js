fc6e68c7b7c342be5136c736e3e651da07e6f318
omarseddati16 <omarseddati16@gmail.com>
2025-07-18 16:08:46 +0200

    bonus

--- js/script.js
+++ js/script.js
@@ -5,10 +5,24 @@ const email = document.getElementById('emailList');
 const apiUrl = 'https://flynn.boolean.careers/exercises/api/random/mail'
 
 // 10 email
-const emails = []
-for (let i = 0; i < 10; i++) {
-  axios.get(apiUrl).then(function (resp) {
-    console.log(emails);
-    email.innerHTML += `<li>${resp.data.response}</li>`;
-  });
+const loadEmails = function () {
+  for (let i = 0; i < 10; i++) {
+    axios.get(apiUrl)
+      .then(function (resp) {
+        email.innerHTML += `<li>${resp.data.response}</li>`;
+        console.log(resp.data.response)
+      });
+  }
 }
+
+// bottone
+const button = document.getElementById('addEmail');
+
+button.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  email.innerHTML = '';
+
+  loadEmails();
+});
+loadEmails();
